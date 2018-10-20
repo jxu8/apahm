66a4ad1c4ba2e0988799e9b0259de813fcd82b71
Ivy Chen <ic2389@columbia.edu>
2018-10-20 19:03:15 -0400

    Cleaning old files

--- src/pages/board.js
+++ src/pages/board.js
@@ -332,7 +332,6 @@ const BoardPage = ({transition}) => (
       <Member
         href=''
         title=''
-        // imgsrc={BoardMembers.Caitlyn}
         name='Caitlyn Chen'
         class='SEAS 2022'
         position='Event Chair'
